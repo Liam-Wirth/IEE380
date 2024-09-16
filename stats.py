0978fdf2147bfe8436df26c4a87330fb16cb96c6
Liam <ltwirth@asu.edu>
2024-09-15 22:49:34 -0700

    lsdkjf

--- stats.py
+++ stats.py
@@ -1,42 +1,87 @@
 #THIS IS A MICROPYTHON FILE
 import math
-def calculate_mean_variance(pmf):
-    """
-    Calculate the mean and variance from a given PMF.
 
-    Parameters:
-    pmf (dict): A dictionary where keys are the values of the random variable
-                and values are the probabilities associated with those values.
 
-    Returns:
-    tuple: A tuple containing the mean and variance.
-    """
-    mean = 0
-    mean_squared = 0
-
-    for x, p in pmf.items():
-        mean += x * p
-        mean_squared += (x ** 2) * p
-
-    variance = mean_squared - (mean ** 2)
-
-    return mean, variance
-
-def uniform_variance(a, b):
+def uniform_variance(a: float, b:float)-> float:
         "Parameters: a, b are the bounds of the uniform distribution"
-    numerator = (b-a + 1)**2 - 1
-    denominator = 12 
-    return numerator / denominator
+        numerator = (b-a + 1)**2 - 1
+        denominator = 12 
+        return numerator / denominator
 
-def uniform_mean(a, b):
+def uniform_mean(a, b) -> float:
     "Parameters: a, b are the bounds of the uniform distribution"
     return (a + b) / 2
-
-
 
 def binomial_mean(n, p):
     return n*p
 
 def binomial_std_dev(n, p):
-    return math.sq
+    return math.sqrt(n*p*(1-p))
+
+def binomial_variance(n, p):
+    return n*p*(1-p)
+
+#if __name__ == "__main__":
+#    main()
+
+def main():
+    usin = None
+    while True:
+        print("Welcome to Liam's stat library")
+        print("0. Distributions")
+        usin = str(input("Choose an option: "))
+
+        match usin:
+            case '0':
+                break
+                # distributions()
+def distributions():
+    print("Distributions")
+    print("0. Uniform")
+    print("1. Binomial")
+    print("2. Poisson")
+    print("3. Geometric")
+    print("4. Bernoulli")
+    print("99. Go Back")
+
+    usin = str(input("Choose an option: "))
+    match usin:
+        case '0' :
+            uniform_distrib()
+        case '1': 
+            # binomials()
+
+        case '2':
+
+            # poisson()
+        case '3':
+
+            # geometric()
+        case '4':
+
+            # bernoulli()
+        case '99':
+            return
+
+
+def uniform_distrib():
+    print("Uniform Distributions:")
+    print("0. Uniform Mean")
+    print("1. Uniform Variance")
+    # TODO: 
+    print("2. Uniform Std Dev")
+
+    usin = str(input("Choose: "))
+    match usin: 
+        case '0':
+            a = float(input("Lower Bound"))
+            b = float(input("Upper Bound"))
+            print(uniform_mean(a,b))
+        case '1':
+
+
+
+        case '2':
+
+        case '3':
 
